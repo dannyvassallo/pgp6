37468ac55c7b79de3b0dc902c8b9b2f5417275b7
Dan Vassallo <danielvassallo87@gmail.com>
2014-11-17 13:15:40 -0500

    Mad Changes

---
+++ source/javascripts/_playlists.js
@@ -0,0 +1,24 @@
+// PLAYLIST 1 //
+
+// variables
+var youtube_tokens,
+$player;
+
+// functions
+var bind_cassette;
+
+//THESE ARE THE YOUTUBE VIDEOS FOR THE LINKS
+youtube_tokens = ["XorEel9J6Ws", "E-Pjdb8_uKw", "odNCtE-I8o4", "tQh7OjYIGWA", "Kr326EmWB5U", "7p_GaQox4EM", "23ySdftuMog", "b6DMIJ_DbF0", "hkq6im9D2xo", "olB7HAIUmgM", "Bxn5th2B-VI", "27UbBjV2B6w", "X0SNKNg9V2A"];
+
+$player = $("div#video-player");
+
+bind_cassette = function(youtube_token, $cassette){
+$cassette.on("click",function(){
+$player.html("<center><div class=\"video-container\"><iframe width=\"460\" height=\"259\" src=\"//www.youtube.com/embed/" + youtube_token + "?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe></div></center>")
+});
+}
+
+$(".tracklistbtn").each(function(i,self){
+bind_cassette(youtube_tokens[i], $(self));
+});
+
